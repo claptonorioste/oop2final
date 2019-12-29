e1efe08cb60789c4361a4e4d2981dbcbdba91746
ianrey258 <45348937+ianrey258@users.noreply.github.com>
2019-12-29 21:25:13 +0800

    crud ragyd taman

---
+++ src/elearning/controller/LoginController.java
@@ -0,0 +1,19 @@
+package elearning.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@SessionScoped
+public class LoginController {
+	private Logger logger = Logger.getLogger(getClass().getName());
+}
